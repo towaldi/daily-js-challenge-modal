b8c5a0c39cb2ab99a169f2fc840bb6770c567531
Tobias Walddörfer <125135663+towaldi@users.noreply.github.com>
2023-11-14 15:47:52 +0100

    add js logic

--- script.js
+++ script.js
@@ -1,80 +1,36 @@
-/**
- * Global variables:
- * -> Counter
- * 
- * -> Html elements
- */
-
-let counter = 0;
-
-const count = document.getElementById('count');
-const numberBgr = document.querySelector('.counter-bar');
-const btns = document.querySelectorAll('.btn');
+const modal = document.getElementById('modal-overlay');
+const openBtn = document.getElementById('modal-open-btn');
+const closeBtn = document.getElementById('modal-close-btn');
 
 
-/**
- * Iterates over all 3 buttons
- * 1. Adds 'click - EventListener' to each button -> event-object as parameter, that contains infomation about 'click event'
- * 2. CSS classes of clicked button saved in 'currentBtnClasses'
- * 3. If statements to check css classes
- */
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(event){
-        currentBtnClasses = event.currentTarget.classList;
-        if (currentBtnClasses.contains('decrease')) {
-            counter--;
-        } else if (currentBtnClasses.contains('reset')) {
-            counter = 0;
-        } else if (currentBtnClasses.contains('increase')) {
-            counter++;
-        }
-        
-        count.textContent = counter;
-        selectColor();
-    })
+// open modal
+openBtn.addEventListener('click', function() {
+        modal.classList.remove('d-none');
+        openBtn.classList.add('d-none');
 });
 
 
-/**
- * Checks the counter 
- * -> counter < 0: red background, counter = 0: black background, counter > 0: green background
- */
-
-function selectColor() {
-    if (counter < 0) {
-        numberBgr.style.backgroundColor = '#cd0000';
-    } else if (counter === 0) {
-        numberBgr.style.backgroundColor = '#000';
-    } else if (counter > 0) {
-        numberBgr.style.backgroundColor = '#03ba00';
-    }
-};
-
-
-
-/*
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(event){
-        currentBtnClasses = event.currentTarget.classList;
-        if (currentBtnClasses.contains('decrease')) {
-            counter--;
-        } else if (currentBtnClasses.contains('reset')) {
-            counter = 0;
-        } else if (currentBtnClasses.contains('increase')) {
-            counter++;
-        }
-
-        if (counter < 0) {
-            numberBgr.style.backgroundColor = '#cd0000';
-        } else if (counter === 0) {
-            numberBgr.style.backgroundColor = '#000';
-        } else if (counter > 0) {
-            numberBgr.style.backgroundColor = '#03ba00';
-        }
-
-        count.textContent = counter;
-        selectColor();
-    })
+// close modal
+closeBtn.addEventListener('click', function() {
+        modal.classList.add('d-none');
+        openBtn.classList.remove('d-none');
 });
 
-*/
+
+
+// version with if statement
+
+// openBtn.addEventListener('click', function() {
+//     if (modal.classList.contains('d-none')) {
+//         modal.classList.remove('d-none');
+//         openBtn.classList.add('d-none');
+//     }
+// });
+
+
+// closeBtn.addEventListener('click', function() {
+//     if (!modal.classList.contains('d-none')) {
+//         modal.classList.add('d-none');
+//         openBtn.classList.remove('d-none');
+//     }
+// });
